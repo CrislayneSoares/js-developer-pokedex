e6e4b705e134835e17062a03743b4e487dc284c0
Crislayne <crissoaresr@hotmail.com>
2023-08-24 11:37:40 -0300

    Mod nos scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,48 @@
-const pokemonList = document.getElementById('pokemonList')
-const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonList = document.getElementById("pokemonsList")
+const loadMoreButtun = document.getElementById("loadMoreButton")
 
 const maxRecords = 151
 const limit = 10
-let offset = 0;
+let offset = 0
 
-function convertPokemonToLi(pokemon) {
-    return `
-        <li class="pokemon ${pokemon.type}">
-            <span class="number">#${pokemon.number}</span>
-            <span class="name">${pokemon.name}</span>
-
-            <div class="detail">
-                <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                </ol>
-
-                <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
-            </div>
-        </li>
-    `
-}
-
-function loadPokemonItens(offset, limit) {
+function loadPokemonItems(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-        const newHtml = pokemons.map(convertPokemonToLi).join('')
+        const newHtml = pokemons.map((pokemon) => 
+        ` 
+            <li class="pokemon ${pokemon.type}">
+                <a href="../assets/pages/card.html">
+                    <span class="number">#${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+
+                    <div class="detail">
+                        <ol>
+                            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                        </ol>
+
+                        <img src="${pokemon.photo}" 
+                            alt="${pokemon.name}">
+                    </div>
+                </a>
+            </li>
+        `
+        ).join('')
         pokemonList.innerHTML += newHtml
     })
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItems(offset, limit)
 
-loadMoreButton.addEventListener('click', () => {
+loadMoreButtun.addEventListener('click', () => {
     offset += limit
-    const qtdRecordsWithNexPage = offset + limit
 
-    if (qtdRecordsWithNexPage >= maxRecords) {
+    const qtdNextPage = offset + limit
+
+    if (qtdNextPage >= maxRecords) {
         const newLimit = maxRecords - offset
-        loadPokemonItens(offset, newLimit)
-
-        loadMoreButton.parentElement.removeChild(loadMoreButton)
+        loadPokemonItems(offset, newLimit)
+        
+        loadMoreButtun.parentElement.removeChild(loadMoreButtun)
     } else {
-        loadPokemonItens(offset, limit)
+        loadPokemonItems(offset, limit)
     }
 })
